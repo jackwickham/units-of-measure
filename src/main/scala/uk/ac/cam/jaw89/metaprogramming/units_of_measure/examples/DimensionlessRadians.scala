56a6d3892ddd8948837442f6d9deba68165564a0
Jack Wickham <jackwickham@live.co.uk>
2019-01-06 15:06:59 +0000

    Improve unsafe radians test

--- src/main/scala/uk/ac/cam/jaw89/metaprogramming/units_of_measure/examples/DimensionlessRadians.scala
+++ src/main/scala/uk/ac/cam/jaw89/metaprogramming/units_of_measure/examples/DimensionlessRadians.scala
@@ -8,12 +8,31 @@ import uk.ac.cam.jaw89.metaprogramming.units_of_measure._
   * Dimensionless values can be silently cast to radians, which can lead to undetected unit errors
   */
 object DimensionlessRadians {
-  val rad: DerivedUnit = DerivedUnit.DimensionlessUnit alias "rad"
-  val deg: DerivedUnit = 2.0 * Math.PI / 360.0 * rad alias "°"
+  object Unsafe {
+    val rad: DerivedUnit = DerivedUnit.DimensionlessUnit alias "rad"
+    val deg: DerivedUnit = 2.0 * Math.PI / 360.0 * rad alias "°"
 
-  def sin(theta: Measurement[Double]): Double = Math.sin(theta.value(rad))
+    def sin(theta: Measurement[Double]): Double = Math.sin(theta.value(rad))
+  }
+  object Safe {
+    val rad: DerivedUnit = defineUnit("rad", BaseDimensions.Dimensionless)
+    val deg: DerivedUnit = 2.0 * Math.PI / 360.0 * rad alias "°"
+
+    def sin(theta: Measurement[Double]): Double = Math.sin(theta.value(rad))
+  }
+
 
   def main(args: Array[String]): Unit = {
-    printf("If units are omitted, %.3f becomes %.3f because the value is treated as radians\n", sin(90.0(deg)), sin(90.0))
+    printf("If units are omitted, %.3f becomes %.3f in the unsafe system, because the value is treated as radians\n",
+           Unsafe.sin(90.0(Unsafe.deg)), Unsafe.sin(90.0))
+    // The safe version still allows degrees to be used
+    Safe.sin(90.0(Safe.deg))
+    // But will throw an exception when unannotated values are used
+    try {
+      Safe.sin(90.0)
+      println("Safe version failed")
+    } catch {
+      case e: NoImplicitConversionsAvailableException => println("The safe version successfully prevented an error")
+    }
   }
 }
--- src/test/scala/uk/ac/cam/jaw89/metaprogramming/units_of_measure/examples/DimensionlessRadiansSpec.scala
+++ src/test/scala/uk/ac/cam/jaw89/metaprogramming/units_of_measure/examples/DimensionlessRadiansSpec.scala
@@ -2,20 +2,34 @@ package uk.ac.cam.jaw89.metaprogramming.units_of_measure.examples
 
 import org.scalatest.Matchers
 import uk.ac.cam.jaw89.metaprogramming.units_of_measure._
-import uk.ac.cam.jaw89.metaprogramming.units_of_measure.examples.DimensionlessRadians.{deg, rad, sin}
+import uk.ac.cam.jaw89.metaprogramming.units_of_measure.examples.DimensionlessRadians.{Unsafe, Safe}
 
 class DimensionlessRadiansSpec extends TestSpec with Matchers {
   private val sinPi_4 = 0.7071067811865476
 
-  "DimensionlessRadians.sin" should "work with radians correctly" in {
-    sin((Math.PI/4)(rad)) should be (sinPi_4 +- 0.05)
+  "DimensionlessRadians.Unsafe.sin" should "work with radians correctly" in {
+    Unsafe.sin((Math.PI/4)(Unsafe.rad)) should be (sinPi_4 +- 0.05)
   }
 
   it should "work with degrees correctly" in {
-    sin(45.0(deg)) should be (sinPi_4 +- 0.05)
+    Unsafe.sin(45.0(Unsafe.deg)) should be (sinPi_4 +- 0.05)
   }
 
   it should "incorrectly allow a value with unspecified units to be passed" in {
-    sin(Math.PI/4) should be (sinPi_4 +- 0.05)
+    Unsafe.sin(Math.PI/4) should be (sinPi_4 +- 0.05)
+  }
+
+  "DimensionlessRadians.Safe.sin" should "work with radians correctly" in {
+    Safe.sin((Math.PI/4)(Safe.rad)) should be (sinPi_4 +- 0.05)
+  }
+
+  it should "work with degrees correctly" in {
+    Safe.sin(45.0(Safe.deg)) should be (sinPi_4 +- 0.05)
+  }
+
+  it should "throw a NoImplicitConversionsAvailableException when a value with unspecified units is passed" in {
+    assertThrows[NoImplicitConversionsAvailableException] {
+      Safe.sin(Math.PI / 4)
+    }
   }
 }
